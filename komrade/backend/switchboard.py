722dfb3899fe79158e500b650c9bf464fb381bbb
quadrismegistus <ryan.heuser@gmail.com>
2020-09-05 22:29:13 +0100

    updates

--- komrade/backend/switchboard.py
+++ komrade/backend/switchboard.py
@@ -88,7 +88,7 @@ class TheSwitchboard(FlaskView, Logger):
             encr_b = b64decode(encr_b64_b)
             self.log('encr_b',encr_b)
             return encr_b
-        except UnicodeDecodeError,binascii.Error:
+        except (UnicodeDecodeError,binascii.Error) as e:
             return OPERATOR_INTERCEPT_MESSAGE
 
         if not encr_b64_str: return OPERATOR_INTERCEPT_MESSAGE
