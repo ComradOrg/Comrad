87e5df6f1d4050b08cf025e1803ecccc241726d9
quadrismegistus <ryan.heuser@gmail.com>
2020-09-05 20:09:30 +0100

    fix

--- komrade/operators/operators.py
+++ komrade/operators/operators.py
@@ -183,12 +183,12 @@ class TheSwitchboard(FlaskView, Logger):
         # return response to caller
         return res
 
-def run_forever():
+def run_forever(port='8080'):
     global OPERATOR
     OPERATOR = TheOperator()
     app = Flask(__name__)
     TheSwitchboard.register(app, route_base='/op/', route_prefix=None)
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, port=port, host='0.0.0.0')
 
 def init_operators():
     op = Operator(name=OPERATOR_NAME)
--- komrade/operators/run_op.py
+++ komrade/operators/run_op.py
@@ -1,1 +1,3 @@
-from operators import run_forever; run_forever()
+import sys
+port = '8080' if len(sys.argv)<2 or not sys.argv[1].isdigit() else sys.argv[1]
+from operators import run_forever; run_forever(port = port)
