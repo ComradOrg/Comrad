ab82b92c0a8d61611c3213f25a1c6c780aaf2eba
quadrismegistus <ryan.heuser@gmail.com>
2020-09-05 22:41:22 +0100

    updates

--- komrade/backend/switchboard.py
+++ komrade/backend/switchboard.py
@@ -120,5 +120,5 @@ def run_forever(port='8080'):
     global OPERATOR
     OPERATOR = TheOperator()
     app = Flask(__name__)
-    TheSwitchboard.register(app, route_base='/op/', route_prefix=None)
+    TheSwitchboard.register(app, route_base='/', route_prefix=None)
     app.run(debug=True, port=port, host='0.0.0.0')
