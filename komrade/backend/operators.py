4ed598a71ed095eec985722403761e31cf5df3d9
quadrismegistus <ryan.heuser@gmail.com>
2020-09-07 21:08:39 +0100

    updates

--- komrade/backend/operators.py
+++ komrade/backend/operators.py
@@ -165,8 +165,11 @@ class Operator(Keymaker):
             encrypted_message_from_caller_to_op,
             encrypted_message_from_caller_to_caller
         ]
+
+        self.log('\n ~~~ \n'.join(MSG_PIECES))
         MSG = BSEP.join(MSG_PIECES)
         MSG_b64 = b64encode(MSG)
+        self.log(' ~~~ ring ring ~~~ rriing ~~~',MSG_b64)
 
         return MSG_b64
 
