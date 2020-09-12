167194d50072b12979498543807a701b64d24704
quadrismegistus <ryan.heuser@gmail.com>
2020-09-12 16:42:59 +0100

    updates

--- komrade/backend/keymaker.py
+++ komrade/backend/keymaker.py
@@ -567,13 +567,13 @@ Keymaker ({self}) is forging new keys for {name}
             if decrypt:
                 encr_key = keychain.get(encr_key_name)
                 self.log(f'about to decrypt {encr_key} with {decr_key}')
-                unencr_key = decr_key.decrypt(encr_key)
-                keychain[unencr_key_name] = get_key_obj(unencr_key_name,unencr_key)
+                unencr_key = decr_key.decrypt(encr_key.data)
+                keychain[unencr_key_name] = get_key_obj(unencr_key_name,unencr_key.data)
             else:
                 unencr_key = keychain.get(unencr_key_name)
                 self.log(f'about to encrypt {unencr_key} with {decr_key}')
-                encr_key = decr_key.encrypt(unencr_key)
-                keychain[encr_key_name] = get_key_obj(encr_key_name,encr_key)
+                encr_key = decr_key.encrypt(unencr_key.data)
+                keychain[encr_key_name] = get_key_obj(encr_key_name,encr_key.data)
 
         return keychain
 
