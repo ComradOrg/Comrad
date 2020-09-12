a169af2bffff74a62a23ec298b207808d39989be
quadrismegistus <ryan.heuser@gmail.com>
2020-09-12 22:22:58 +0100

    updates

--- komrade/backend/operators.py
+++ komrade/backend/operators.py
@@ -57,6 +57,7 @@ class Operator(Keymaker):
                          path_crypt_keys=path_crypt_keys, path_crypt_data=path_crypt_data)
         
         self.find_pubkey_and_name(name,pubkey)
+        print('booted with operator with:',self.name,self.pubkey,'??')
         
     # def boot(self):
     #     ## get both name and pubkey somehow
