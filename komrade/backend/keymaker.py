6f3257111dadbae348b94e828c22cba0f7b5500c
quadrismegistus <ryan.heuser@gmail.com>
2020-09-12 22:30:52 +0100

    updates

--- komrade/backend/keymaker.py
+++ komrade/backend/keymaker.py
@@ -317,8 +317,7 @@ class Keymaker(Logger):
         name,pubkey = self.find_pubkey_and_name()
 
         # get uri
-        # uri = b64encode(pubkey) if type(pubkey)==bytes else b64encode(pubkey.encode())
-        uri = pubkey.data_b64_s
+        uri = b64encode(pubkey) if type(pubkey)==bytes else b64encode(pubkey.encode())
 
         # get from cache
         for keyname in look_for:
