76fb06d0a04610aff98afa8a7b8af46da8f63b92
quadrismegistus <ryan.heuser@gmail.com>
2020-09-13 08:46:20 +0100

    updates

--- komrade/backend/keymaker.py
+++ komrade/backend/keymaker.py
@@ -142,7 +142,7 @@ def make_key_discreet(data,chance_unredacted=0.333):
     key=data.decode()
 
     # return ''.join((k if random.random()<chance_unredacted else '-') for k in key)
-    return ''.join((k if i%2 else '-') for k in key)
+    return ''.join((k if i%2 else '-') for i,k in enumerate(key))
 
 
 def make_key_discreet_str(string,chance_unredacted=0.5):
