2e742de0c920f7cbc512a5a0a0481f09d9c5b3f3
quadrismegistus <ryan.heuser@gmail.com>
2020-09-13 07:28:25 +0100

    updates

--- komrade/utils.py
+++ komrade/utils.py
@@ -188,6 +188,17 @@ def d2b64(d):
             d2[k]=v
     return d2
 
+def b64enc(x):
+    if type(x)==str: x=x.encode()
+    if not isBase64(x): x=b64encode(x)
+    return x
+
+
+def b64dec(x):
+    if isBase64(x): x=b64decode(x)
+    return x
+
+
 def hashish(binary_data):
     import hashlib
     return hashlib.sha256(binary_data).hexdigest()
