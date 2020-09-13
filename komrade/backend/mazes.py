1b09ded101cb0a36182515136861720f862609cd
quadrismegistus <ryan.heuser@gmail.com>
2020-09-13 19:07:35 +0100

    updates

--- komrade/backend/mazes.py
+++ komrade/backend/mazes.py
@@ -166,9 +166,9 @@ from torpy.hiddenservice import HiddenService
 from torpy.http.requests import TorRequests, tor_requests_session, do_request as requests_request
 from torpy.http.urlopener import do_request as urllib_request
 
-logging.getLogger('requests').setLevel(logging.CRITICAL)
-logging.basicConfig(format='[%(asctime)s] [%(threadName)-16s] %(message)s', level=logging.DEBUG)
-logger = logging.getLogger('komrade')
+# logging.getLogger('requests').setLevel(logging.CRITICAL)
+# logging.basicConfig(format='[%(asctime)s] [%(threadName)-16s] %(message)s', level=logging.DEBUG)
+# logger = logging.getLogger('komrade')
 
 
 HS_BASIC_HOST = os.getenv('HS_BASIC_HOST')
