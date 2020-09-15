3064a331af3d653c0636c985a487cb6ae65dcbb8
quadrismegistus <ryan.heuser@gmail.com>
2020-09-15 11:39:49 +0100

    updates

--- komrade/backend/messages.py
+++ komrade/backend/messages.py
@@ -50,13 +50,17 @@ class Message(Logger):
 
         fpk=b64enc_s(self.from_pubkey)
         fpk1,fpk2 = fpk[:len(fpk)//2],fpk[len(fpk)//2:]
+        numchar = len(b64enc_s(self.to_pubkey))
+        linestr = '-'*numchar
 
         return f"""    
     from: @{self.from_name if self.from_name else ''} 
           [{b64enc_s(self.from_pubkey)}]
 
     to:   @{self.to_name if self.to_name else ''}
-          ^{b64enc_s(self.to_pubkey)}]
+          [{b64enc_s(self.to_pubkey)}]
+
+    ------{linestr}
 
     msg:  {msg}
 """
