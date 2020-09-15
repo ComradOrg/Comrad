45a98a68bf326e14a221356831c8198bac27b939
quadrismegistus <ryan.heuser@gmail.com>
2020-09-15 08:50:39 +0100

    updates

--- komrade/backend/the_operator.py
+++ komrade/backend/the_operator.py
@@ -390,7 +390,7 @@ from_komrade = {from_komrade}
         self.log('reloaded inbox:',inbox_old)
 
         # add new inbox
-        inbox_new = post_id #+ (BSEP+inbox_old if inbox_old else b'')
+        inbox_new = post_id + (BSEP+inbox_old if inbox_old else b'')
         self.log('new inbox = ',inbox_new)
 
         # reencrypt
