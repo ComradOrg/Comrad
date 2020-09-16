325a21652ff1e9125923ec451cc63cff67bef0d3
quadrismegistus <ryan.heuser@gmail.com>
2020-09-16 16:22:56 +0100

    updates

--- komrade/backend/komrades.py
+++ komrade/backend/komrades.py
@@ -107,7 +107,7 @@ class KomradeX(Caller):
         ## 2) Make pub public/private keys
         keypair = KomradeAsymmetricKey()
         pubkey,privkey = keypair.pubkey_obj,keypair.privkey_obj
-        logfunc(f'Using the iron-clad Elliptic curve algorithm, I have cut for you a private and public asymmetric key pair:\n\n(1) {pubkey}\n\n(2) {privkey}{ART_KEY_PAIR}',clear=False,pause=True)
+        logfunc(f'Using the iron-clad Elliptic curve algorithm, I have cut for you a private and public asymmetric key pair:\n\n(1) {pubkey}\n\n(2) {privkey}{ART_KEY_PAIR}',clear=True,pause=True)
 
         ## 3) Have passphrase?
         if SHOW_STATUS and not passphrase:
