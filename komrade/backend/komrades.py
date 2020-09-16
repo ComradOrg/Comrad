fad6654f5e25eea05a13f5efb9ffd32ed8444d03
quadrismegistus <ryan.heuser@gmail.com>
2020-09-16 16:20:54 +0100

    updates

--- komrade/backend/komrades.py
+++ komrade/backend/komrades.py
@@ -107,7 +107,7 @@ class KomradeX(Caller):
         ## 2) Make pub public/private keys
         keypair = KomradeAsymmetricKey()
         pubkey,privkey = keypair.pubkey_obj,keypair.privkey_obj
-        logfunc(f'I have cut for you a private and public asymmetric key pair using the Elliptic Curve algorithm from Themis cryptography library:\n\n(1) {pubkey}\n\n(2) {privkey}{ART_KEY_PAIR}',clear=False,pause=True)
+        logfunc(f'I have cut for you a private and public asymmetric key pair.\n\nI used the iron-clad Elliptic Curve algorithm, as implemented by Themis cryptography library:\n\n(1) {pubkey}\n\n(2) {privkey}{ART_KEY_PAIR}',clear=False,pause=True)
 
         ## 3) Have passphrase?
         if SHOW_STATUS and not passphrase:
