7ae61cff4a405efbe208a13bc2188323765bef8a
quadrismegistus <ryan.heuser@gmail.com>
2020-09-17 16:32:19 +0100

    komrade update!

--- komrade/backend/keymaker.py
+++ komrade/backend/keymaker.py
@@ -505,7 +505,7 @@ class Keymaker(Logger):
                     encr_key = decr_key.encrypt(unencr_key.data)
                     keychain[encr_key_name] = get_key_obj(encr_key_name,encr_key)
             except ThemisError as e:
-                #exit('Incorrect password.')
+                exit('Incorrect password.')
                 #self.log('error!!',e,decrypt,decr_key,encr_key,decr_key_name,encr_key_name)
                 pass
 
