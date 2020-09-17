2fce5a04283eb1ed0c6cdce2623179d446608bf3
quadrismegistus <ryan.heuser@gmail.com>
2020-09-17 17:33:31 +0100

    komrade update!

--- komrade/backend/komrades.py
+++ komrade/backend/komrades.py
@@ -455,10 +455,13 @@ class KomradeX(Caller):
 
         # update posts
         for post_id,post in id2post.items():
+            self.log(f'saving post!\n{post_id}\n\n{post}')
+
             self.crypt_data.set(
                 post_id,
                 post,
-                prefix=post_prefix
+                prefix=post_prefix,
+                override=True
             )
             
         res = {
