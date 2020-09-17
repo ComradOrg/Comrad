259086c5af7b39d063fb73e67e6c9d6d4a2a70c0
quadrismegistus <ryan.heuser@gmail.com>
2020-09-17 20:13:51 +0100

    komrade update!

--- komrade/backend/komrades.py
+++ komrade/backend/komrades.py
@@ -505,12 +505,19 @@ class KomradeX(Caller):
 
         # (2) save msgs
         id2msg=res.get('res_msgs').get('posts',{})
-        self.log(f'downloaded {len(id2msg)} messages:',list(id2msg.keys()))
-        self.save_msgs(id2msg)
         
         # (3) save posts
         id2post=res.get('res_posts').get('posts',{})
+        
+        # save them: but posts arent msgs!
+        # @hack! why is this happening?
+
+
+        id2msg = dict([(k,v) for k,v in id2msg.items() if k not in id2post])
+        self.log(f'downloaded {len(id2msg)} messages:',list(id2msg.keys()))
         self.log(f'downloaded {len(id2post)} posts:',list(id2post.keys()))
+
+        self.save_msgs(id2msg)
         self.save_posts(id2post)
 
         return res
