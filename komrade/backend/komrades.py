2eae553239609615bedffa48d68f96b88497bbd8
quadrismegistus <ryan.heuser@gmail.com>
2020-09-17 07:52:40 +0100

    komrade update!

--- komrade/backend/komrades.py
+++ komrade/backend/komrades.py
@@ -344,7 +344,7 @@ class KomradeX(Caller):
         # get binary blob for all fetched posts
         msgs_d = msg_from_world.msg
         self.log('msgs_d??',msgs_d)
-        msgs = msgs_d['msg'].split(BSEP)
+        msgs = msgs_d['msg'].split(BSEP) if msgs['msg'] else []
 
         res = {
             'status':'Fetched {len(msgs)} poss.',
