d0a6ccd624705d808d6824c55e9b8a6120c5785a
quadrismegistus <ryan.heuser@gmail.com>
2020-09-21 14:40:24 +0100

    updates

--- komrade/constants.py
+++ komrade/constants.py
@@ -1,7 +1,7 @@
 # addresses
-KOMRADE_URL = '68.66.241.111'
 KOMRADE_ONION = 'u7spnj3dmwumzoa4.onion'
 KOMRADE_ONION2 = 'rwg4zcnpwshv4laq.onion'
+KOMRADE_URL = KOMRADE_ONION
 
 
 OPERATOR_API_URL = f'http://{KOMRADE_ONION}/op/'
